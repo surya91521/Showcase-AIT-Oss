de5a47dcf77decc717f1623860729d2b79d04592
fang009 <surya91521@gmail.com>
2020-11-05 00:23:18 +0530

    animation added to hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,22 +2,27 @@ import React from 'react';
 import {Button} from "./Button";
 import './HeroSection.css';
 import '../App.css';
-import pdf from '../assets/intern.pdf';
+import Fade from 'react-reveal/Fade'
+import ReactRoundedImage from "react-rounded-image";
+import profile from '../xx.jpg';
+
 
 function HeroSection() {
     return (
-        <div className = 'hero-container' style ={{ 'background': 'url('+process.env.PUBLIC_URL + '/assets/images/img-home.jpg) center center/cover no-repeat'}} >
-            <div className = 'info'>
-         {/* <video src='video-2.mp4' autoPlay loop muted /> */}
-            <h1>S Suryanarayanan</h1>
-             <p>asajaskjAKSJAKJSAKJSLKAsjlaks
-                 ASaKJNSJKANSAKNskaNSKANKSAKsAKSM
-                 SASbajsnkANSKANSKAsnlAKNSLAKNslak</p>
-                
-            </div>   
-            <p>Lets make this  a Success!</p>
-            <div className="hero-btns">
+        <div className = 'hero-container' style ={{ 'background': 'url('+process.env.PUBLIC_URL + '/assets/images/night.jpg) center center/cover no-repeat'}} >
             
+         {/* <video src='video-2.mp4' autoPlay loop muted /> */}
+        <Fade top>
+         < ReactRoundedImage 
+            image ={profile}
+          roundedColor="#FFFFFF"
+          imageWidth="300"
+          imageHeight="300"
+          roundedSize="5"
+           />
+            <p>S Suryanarayanan</p>
+            
+            <div className="hero-btns">
               <a className ="btns" href={process.env.PUBLIC_URL+'/assets/intern.pdf'} download> 
 
               <Button className ="btns" 
@@ -26,13 +31,19 @@ function HeroSection() {
                  My Resume     
                 </Button>
                 </a>   
-              
+            
              
 
             </div>
+            </Fade >
 
         </div>
     )
+
+   
 }
+
+  
+
 
 export default HeroSection; 
