f81888e6ff048ce78d25675e7963e252504a7502
fang009 <surya91521@gmail.com>
2020-11-04 21:05:30 +0530

    resume download done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,36 +9,36 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/skills.jpg'
+              src='/assets/images/skills.jpg'
               text='Skills'
               path='/skills'
             />
             <CardItem
-              src='images/projects.jpg'
+              src='/assets/images/projects.jpg'
               text='My Projects'
               path='/projects'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/courses.jpg'
+              src='/assets/images/courses.jpg'
               text='Courses Certificates'
               path='/courses'
             />
             <CardItem
-              src='images/coding.jpg'
+              src='/assets/images/coding.jpg'
               text='Coding and Blogs'
               path='/coding'
             />
           </ul>
           <ul className='cards__items'>
           <CardItem
-              src='images/awards.jpg'
+              src='/assets/images/awards.jpg'
               text='Award and Honours'
               path='/awards'
             />
                 <CardItem
-              src='images/intern.jpg'
+              src='/assets/images/intern.jpg'
               text='Internships'
               path='/internships'
             />
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,20 +2,32 @@ import React from 'react';
 import {Button} from "./Button";
 import './HeroSection.css';
 import '../App.css';
+import pdf from '../assets/intern.pdf';
 
 function HeroSection() {
     return (
-        <div className = 'hero-container'>
-         <video src='video-2.mp4' autoPlay loop muted />
+        <div className = 'hero-container' style ={{ 'background': 'url('+process.env.PUBLIC_URL + '/assets/images/img-home.jpg) center center/cover no-repeat'}} >
+            <div className = 'info'>
+         {/* <video src='video-2.mp4' autoPlay loop muted /> */}
             <h1>S Suryanarayanan</h1>
+             <p>asajaskjAKSJAKJSAKJSLKAsjlaks
+                 ASaKJNSJKANSAKNskaNSKANKSAKsAKSM
+                 SASbajsnkANSKANSKAsnlAKNSLAKNslak</p>
+                
+            </div>   
             <p>Lets make this  a Success!</p>
             <div className="hero-btns">
+            
+              <a className ="btns" href={process.env.PUBLIC_URL+'/assets/intern.pdf'} download> 
+
               <Button className ="btns" 
-               buttonStyle='btn--outline'
-              buttonSize='btn--large'>
-              <a className ="btns" href="intern.pdf" download = "Resume.pdf"> </a>   
-               My Resume 
-              </Button>
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'>
+                 My Resume     
+                </Button>
+                </a>   
+              
+             
 
             </div>
 
