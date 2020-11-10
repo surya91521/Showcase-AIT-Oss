42cee636fe1a19687c793859e6b9c56a60e1f8bc
fang009 <surya91521@gmail.com>
2020-11-10 20:54:09 +0530

    all changes done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import emailjs from 'emailjs-com'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -9,9 +10,23 @@ import {
   faLinkedinIn
 } from "@fortawesome/free-brands-svg-icons";
 
+function sendEmail(e){
+
+      e.preventDefault();
+
+      emailjs.sendForm('gmail', 'template_argvovo', e.target, 'user_CWrEbev5HvwxbNsKP6nh1')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+
+}
+
 function Footer() {
   return (
-    <div className='footer-container'>
+    <div className='footer-container' style={{paddingBottom:"30px"}}>
       <h3 style={{fontFamily:"serif"}}>Follow Me On</h3>
    
       <div className="social-container">
@@ -43,6 +58,25 @@ function Footer() {
        Phone No : +91 8427516566<br/><br />
       Email Id : surya91521@gmail.com
       </p>
+      <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
       </div>
     </div>
   );
